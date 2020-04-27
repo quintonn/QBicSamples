49430d55c7fd146eb3ce1764066a81e4457a5302
Vasile <lv16102019@gmail.com>
2020-04-27 11:20:23 +0300

    GetInput to CoreView in progress

--- QBicSamples/Samples/MultipleViews/Editions/ModifyEdition.cs
+++ QBicSamples/Samples/MultipleViews/Editions/ModifyEdition.cs
@@ -1,61 +1,28 @@
-﻿using QBicSamples.Models;
+﻿using NHibernate;
+using QBicSamples.Models;
 using QBicSamples.SiteSpecific;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.ServiceModel.Channels;
 using System.Threading.Tasks;
 using WebsiteTemplate.Backend.Services;
 using WebsiteTemplate.Menus;
 using WebsiteTemplate.Menus.BaseItems;
 using WebsiteTemplate.Menus.InputItems;
 using WebsiteTemplate.Menus.ViewItems.CoreItems;
-using WebsiteTemplate.Utilities;
 
 namespace QBicSamples.Samples.MultipleViews.Editions
 {
     public abstract class ModifyEdition : CoreModify<Edition>
     {
-        public string EditionId;
-
-        public string ModelId;
-
-        public string ManufacturerId;
         public ModifyEdition(DataService dataService, bool isNew) : base(dataService, isNew)
         {
         }
-        private Edition Edition { get; set; }
-        public override bool AllowInMenu => false;
+
         public override string EntityName => "Edition";
+
         public override EventNumber GetViewNumber()
         {
             return MenuNumber.ViewEditions;
-        }
-        public override async Task<InitializeResult> Initialize(string data)
-        {
-            var json = JsonHelper.Parse(data);
-
-            EditionId = json.GetValue("Id");
-            ModelId = json.GetValue("ModelId");
-            ManufacturerId = json.GetValue("ManufacturerId");
-
-            IsNew = String.IsNullOrWhiteSpace(EditionId);
-            if (IsNew)
-            {
-                Edition = new Edition();
-                Edition.ManufacturerId = ManufacturerId;
-                Edition.ModelId = ModelId;
-                Edition.EditionYear = DateTime.ParseExact("1990-01-01", "yyyy-MM-dd", CultureInfo.InvariantCulture);
-            }
-            else
-            {
-                using (var session = DataService.OpenSession())
-                {
-                    Edition = session.Get<Edition>(EditionId);
-                }
-            }
-
-            return new InitializeResult(true);
         }
 
         public override List<InputField> InputFields()
@@ -69,81 +36,43 @@ namespace QBicSamples.Samples.MultipleViews.Editions
 
             return result;
         }
-        public abstract Task<IList<IEvent>> PerformModify(bool isNew, string id, ISession session);
-        public override async Task<IList<IEvent>> ProcessAction(int actionNumber)
+
+        public override async Task<IList<IEvent>> PerformModify(bool isNew, string id, ISession session)
         {
-            if (actionNumber == 1) // User clicked cancel
+            var name = GetValue("EditionName");
+            var year = GetValue<DateTime>("EditionYear");
+            var manufacturerId = GetValue("ManufacturerId");
+            var modelId = GetValue("ModelId");
+
+            if (String.IsNullOrEmpty(name))
             {
                 return new List<IEvent>()
-                {
-                    new CancelInputDialog()
-                };
-            }
-            else if (actionNumber == 0) // user clicked submit
-            {
-                var id = GetValue("Id");
-                var name = GetValue("EditionName");
-                var year = GetValue<DateTime>("EditionYear");
-                var manufacturerId = GetValue("ManufacturerId");
-                var modelId = GetValue("ModelId");
-
-                if (String.IsNullOrEmpty(name))
-                {
-                    return new List<IEvent>()
                     {
                         new ShowMessage("Name is mandatory")
                     };
-                }
+            }
 
-                using (var session = DataService.OpenSession())
-                {
-                    Edition dbItem;
-                    if (IsNew)
-                    {
-                        dbItem = new Edition();
-                        dbItem.ManufacturerId = manufacturerId;
-                        dbItem.ModelId = modelId;
-                    }
-                    else
-                    {
-                        dbItem = session.Get<Edition>(id);
-                    }
+            Edition edition;
 
-                    if (!String.IsNullOrEmpty(name))
-                    {
-                        dbItem.EditionName = name;
-                        dbItem.EditionYear = year;
-                    }
-
-                    DataService.SaveOrUpdate(session, dbItem); // Using saveOrUpdate adds this task to audit log
-                    //session.SaveOrUpdate(dbItem); // this won't be audited
-
-                    session.Flush();
-                }
-
-
-                var data = new
-                {
-                    data = new
-                    {
-                        Id = ModelId,
-                        ManufacturerId,
-                    }
-                };
-                var json = JsonHelper.SerializeObject(data);
-
-
-                return new List<IEvent>()
-                {
-                    new ShowMessage("Edition successfully " + (IsNew ? "added" : "modified")),
-                    new CancelInputDialog(),
-                    new ExecuteAction(MenuNumber.ViewEditions, json)
-                };
+            if (isNew)
+            {
+                edition = new Edition();
+                edition.ManufacturerId = manufacturerId;
+                edition.ModelId = modelId;
             }
             else
             {
-                return null;
+                edition = session.Get<Edition>(id);
             }
+            if (!String.IsNullOrEmpty(name))
+            {
+                edition.EditionName = name;
+                edition.EditionYear = year;
+            }
+
+            DataService.SaveOrUpdate(session, edition);
+
+            return null;
         }
     }
 }
--- QBicSamples/Samples/MultipleViews/Models/ModifyModel.cs
+++ QBicSamples/Samples/MultipleViews/Models/ModifyModel.cs
@@ -1,4 +1,5 @@
-﻿using QBicSamples.Models;
+﻿using NHibernate;
+using QBicSamples.Models;
 using QBicSamples.SiteSpecific;
 using System;
 using System.Collections.Generic;
@@ -7,135 +8,77 @@ using WebsiteTemplate.Backend.Services;
 using WebsiteTemplate.Menus;
 using WebsiteTemplate.Menus.BaseItems;
 using WebsiteTemplate.Menus.InputItems;
+using WebsiteTemplate.Menus.ViewItems.CoreItems;
 using WebsiteTemplate.Utilities;
 
 namespace QBicSamples.Samples.MultipleViews.Models
 {
-    public abstract class ModifyModel : GetInput
+    public abstract class ModifyModel : CoreModify<VehicleModel>
     {
-        private bool IsNew { get; set; }
-
+        private VehicleModel Model { get; set; }
+        
         public string ModelId;
 
         public string ManufacturerId;
-        public ModifyModel(DataService dataService, bool isNew)
+        public ModifyModel(DataService dataService, bool isNew) : base(dataService, isNew)
         {
-            DataService = dataService;
-            IsNew = isNew;
         }
+        public override string EntityName => "Vehicle Model";
+        public override EventNumber GetViewNumber()
+        {
+            return MenuNumber.ViewModels;
+        }
+        public override List<InputField> InputFields()
+        {
+            var result = new List<InputField>();
 
-        private VehicleModel Model { get; set; }
+            result.Add(new StringInput("Name", "Name", Item?.Name, null, true));
+            result.Add(new HiddenInput("ManufacturerId", Item?.ManufacturerId));
 
-        private DataService DataService { get; set; }
-
-        public override bool AllowInMenu => false;
-
-        public override string Description => IsNew ? "Add Model" : "Edit Model";
+            return result;
+        }
 
         public override async Task<InitializeResult> Initialize(string data)
         {
             var json = JsonHelper.Parse(data);
 
-            ModelId = json.GetValue("Id");
             ManufacturerId = json.GetValue("ManufacturerId");
-
-            IsNew = String.IsNullOrWhiteSpace(ModelId);
-            if (IsNew)
-            {
-                Model = new VehicleModel();
-                Model.ManufacturerId = ManufacturerId;
-            }
-            else
-            {
-                using (var session = DataService.OpenSession())
-                {
-                    Model = session.Get<VehicleModel>(ModelId);
-                }
-            }
 
             return new InitializeResult(true);
         }
-
-        public override IList<InputField> GetInputFields()
+        public override async Task<IList<IEvent>> PerformModify(bool isNew, string id, ISession session)
         {
-            var result = new List<InputField>();
+            var name = GetValue("Name");
 
-            result.Add(new HiddenInput("Id", Model?.Id)); // Need to add it so it's available when doing update
-            result.Add(new HiddenInput("ManufacturerId", Model?.ManufacturerId));
-            result.Add(new StringInput("Name", "Name", Model?.Name, null, true));
+            VehicleModel model;
 
-            return result;
-        }
-
-        public override async Task<IList<IEvent>> ProcessAction(int actionNumber)
-        {
-            if (actionNumber == 1) // User clicked cancel
+            if (isNew)
             {
-                return new List<IEvent>()
-                {
-                    new CancelInputDialog()
-                };
-            }
-            else if (actionNumber == 0) // user clicked submit
-            {
-                var id = GetValue("Id");
-                var name = GetValue("Name");
-                var manufacturerId = GetValue("ManufacturerId");
-
-                if (String.IsNullOrEmpty(name))
-                {
-                    return new List<IEvent>()
-                    {
-                        new ShowMessage("Name is mandatory")
-                    };
-                }
-
-                using (var session = DataService.OpenSession())
-                {
-                    VehicleModel dbItem;
-                    if (IsNew)
-                    {
-                        dbItem = new VehicleModel();
-                        dbItem.ManufacturerId = manufacturerId;
-                    }
-                    else
-                    {
-                        dbItem = session.Get<VehicleModel>(id);
-                    }
-
-                    if (!String.IsNullOrEmpty(name))
-                    {
-                        dbItem.Name = name;
-                    }
-
-                    DataService.SaveOrUpdate(session, dbItem); // Using saveOrUpdate adds this task to audit log
-                    //session.SaveOrUpdate(dbItem); // this won't be audited
-
-                    session.Flush();
-                }
-
-
-                var data = new
-                {
-                    data = new
-                    {
-                        Id = ManufacturerId  
-                    }
-                };
-                var json = JsonHelper.SerializeObject(data);
-
-
-                return new List<IEvent>()
-                {
-                    new ShowMessage("Model successfully " + (IsNew ? "added" : "modified")),
-                    new CancelInputDialog(),
-                    new ExecuteAction(MenuNumber.ViewModels, json)
-                };
+                model = new VehicleModel();
+                model.ManufacturerId = ManufacturerId;
             }
             else
             {
-                return null;
+                model = session.Get<VehicleModel>(id);
             }
+
+            model.Name = name;
+
+            DataService.SaveOrUpdate(session, model);
+
+            var data = new
+            {
+                data = new
+                {
+                    Id = ManufacturerId
+                }
+            };
+            var json = JsonHelper.SerializeObject(data);
+            return new List<IEvent>()
+                {
+                    new CancelInputDialog(),
+                    new ExecuteAction(MenuNumber.ViewModels, json),
+                };
         }
     }
 }
